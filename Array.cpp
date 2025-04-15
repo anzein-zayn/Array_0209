9b8c4257619264517ef471847f5f2697c739b533
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2025-04-15 13:11:59 +0700

    Membuat main dan temporary array

--- Array.cpp
+++ Array.cpp
@@ -1,2 +1,15 @@
 #include<iostream>
 using namespace std;
+
+//Create main and temporary array
+int arr[20], B[30];
+int n;
+
+void input() {
+    while (true){
+        cout << "Enter the Number of Elements ";
+        for (int i = 0;i < n; i++);
+        cin >> arr[i];
+    }
+}
+
